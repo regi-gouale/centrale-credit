312030886c403434f60b120a60e92b4ea92cb192
Régi GOUALE <74569854+regi-gouale@users.noreply.github.com>
2023-06-18 05:17:13 +0200

    create 3 files and update 1 file

---
+++ migrate.ts
@@ -0,0 +1,54 @@
+import { Sequelize } from 'sequelize-typescript';
+const { Umzug, SequelizeStorage } = require('umzug');
+
+const sequelize = new Sequelize({
+    dialect: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: '',
+    password: '',
+    database: 'centralecredit'
+});
+
+const umzug = new Umzug({
+    storage: new SequelizeStorage({ sequelize }),
+    storageOptions: { sequelize },
+    logging: false,
+    migrations: {
+        params: [
+            sequelize,
+            sequelize.constructor,
+        ],
+        path: './src/migrations',
+        pattern: /\.ts$/,
+    },
+});
+
+const task = (process.argv[2] || '').trim();
+
+switch (task) {
+    case 'up':
+        umzug.up().then((migrations) => {
+            console.log('Migrations Up performed!', migrations);
+            process.exit(0);
+        }
+        ).catch((err) => {
+            console.log('Error performing migrations', err);
+            process.exit(1);
+        }
+        );
+        break;
+    case 'down':
+        umzug.down().then((migrations) => {
+            console.log('Migrations Down performed!', migrations);
+            process.exit(0);
+        }
+        ).catch((err) => {
+            console.log('Error performing migrations', err);
+            process.exit(1);
+        }
+        );
+        break;
+    default:
+        break;
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './modules/database/database.module';
 
 @Module({
-  imports: [],
+  imports: [
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
